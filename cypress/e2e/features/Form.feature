77b975ac28b5b6b5973d24fa424759e04b2042a5
enderta <endertanver@gmail.com>
2024-02-06 17:55:08 +0000

    form

--- cypress/e2e/features/Form.feature
+++ cypress/e2e/features/Form.feature
@@ -3,11 +3,13 @@ Feature:  Volunteer Application Form
   Scenario Outline: Filling out the form with valid details
     Given  I am on the volunteer application form page
     When  I fill in the personal details with "<FirstName>", "<LastName>", "<Email>", and "<Phone>", "<City>"
-    Then I fill the test area with "<TestArea>" and "<Options>"
+    Then I fill the test area with "<TestArea>"
+    Then I select the options "<Team>", "<Industry>", "<Hear>"
+    Then I click on the "<Guide>" and "<Teach people coding>" as some experience
     And I approve the agreement
     And I submit the form
     Then I should see "<ConfirmationMessage>"
 
     Examples:
-      | FirstName | LastName | Email       | Phone       | City  | TestArea           | Options                                                                 | ConfirmationMessage           |
-      | John      | Doe      | jd@gmail.com| 123-456-7890| London| I want to help     | Team, industry, Hear, Guide, Teach, Help, Education, Social Media, Coaching, JavaScript, Project Management | Thank you for applying |
+      | FirstName | LastName | Email        | Phone        | City   | TestArea       | Team      | Industry | Hear         | Guide                | Teach people coding | ConfirmationMessage    |
+      | John      | Doe      | jd@gmail.com | 123-456-7890 | London | I want to help | Education | Education | Social media | Coaching / Mentoring | React               | Thank you for applying |
--- cypress/e2e/step_definitions/Form.steps.js
+++ cypress/e2e/step_definitions/Form.steps.js
@@ -3,7 +3,7 @@ const {When, Then, Given} = require("@badeball/cypress-cucumber-preprocessor");
 let firstName, lastName, email, phone, city, testArea, options, confirmationMessage;
 
     Given("I am on the volunteer application form page", function () {
-        cy.visit("http://localhost:3002/"); // replace with the URL of your volunteer application form page
+        cy.visit("http://localhost:3000/"); // replace with the URL of your volunteer application form page
     });
 
 When("I fill in the personal details with {string}, {string}, {string}, and {string}, {string}", function (string, string2, string3, string4, string5) {
@@ -16,21 +16,37 @@ When("I fill in the personal details with {string}, {string}, {string}, and {str
 
 });
 
-Then('I fill the test area with "{string}" and "{string}"', (tArea, opts) => {
-    testArea = tArea;
-    options = opts;
-    // code to fill the test area
+Then("I fill the test area with {string}", function (string) {
+    cy.get('#interestedInVolunteer').type(string);
+    cy.get('#interestedInCYF').type(string);
+});
+
+Then("I select the options {string}, {string}, {string}", function (string, string2, string3) {
+    cy.get('#teamId').select(string);
+    cy.get('#industry').select(string2);
+    cy.get('#hearAboutCYF').select(string3);
+});
+
+Then("I click on the {string} and {string} as some experience", function (string, string2) {
+    cy.get(':nth-child(10) > :nth-child(2) > .form-table-single > .form-check > .form-check-label').click();
+    cy.get('[for="media98y439hf934Some"]').click();
+    cy.get('#media23edcs3h3j3').click();
+    cy.get('#media23edcs3h3j3').click();
 });
 
 Then('I approve the agreement', () => {
-    // code to approve the agreement
+    cy.get('#agreeToTOU').click();
+    cy.get('#agreeToReceiveCommunication').click();
 });
 
 Then('I submit the form', () => {
-    // code to submit the form
+    cy.get('.volunteer-submit-btn').click();
 });
 
-Then('I should see "{string}"', (cMessage) => {
-    confirmationMessage = cMessage;
-    // code to check the confirmation message
+Then("I should see {string}", function (string) {
+    cy.get('#root > div > div.main > div > p').then(($p) => {
+        const text = $p.text();
+        console.log(text);
+        expect(text).to.include(string);
+    })
 });
