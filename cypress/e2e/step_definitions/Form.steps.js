f1f9490bbb502878e4b172617d393f6db1e959c7
enderta <endertanver@gmail.com>
2024-02-07 12:41:02 +0000

    form

--- cypress/e2e/step_definitions/Form.steps.js
+++ cypress/e2e/step_definitions/Form.steps.js
@@ -2,32 +2,30 @@ const {When, Then, Given} = require("@badeball/cypress-cucumber-preprocessor");
 
 let firstName, lastName, email, phone, city, testArea, options, confirmationMessage;
 
-    Given("I am on the volunteer application form page", function () {
-        cy.visit("http://localhost:3000/"); // replace with the URL of your volunteer application form page
-    });
-
-When("I fill in the personal details with {string}, {string}, {string}, and {string}, {string}", function (string, string2, string3, string4, string5) {
-    cy.get('#firstName').type(string);
-    cy.get('#lastName').type(string2);
-    cy.get('#email').type(string3);
-    cy.get('#phone-number-input').type(string4);
-    cy.get('#cityId').select(string5);
-
-
+Given("I am on the volunteer application form page", function () {
+    cy.visit("http://localhost:3000/");
 });
 
-Then("I fill the test area with {string}", function (string) {
-    cy.get('#interestedInVolunteer').type(string);
-    cy.get('#interestedInCYF').type(string);
+When("I fill in the personal details with {string}, {string}, {string}, and {string}, {string}", function (FirstName, LastName, Email, Phone, City) {
+    cy.get('#firstName').type(FirstName);
+    cy.get('#lastName').type(LastName);
+    cy.get('#email').type(Email);
+    cy.get('#phone-number-input').type(Phone);
+    cy.get('#cityId').select(City);
 });
 
-Then("I select the options {string}, {string}, {string}", function (string, string2, string3) {
-    cy.get('#teamId').select(string);
-    cy.get('#industry').select(string2);
-    cy.get('#hearAboutCYF').select(string3);
+Then("I fill the test area with {string}", function (TestArea) {
+    cy.get('#interestedInVolunteer').type(TestArea);
+    cy.get('#interestedInCYF').type(TestArea);
 });
 
-Then("I click on the {string} and {string} as some experience", function (string, string2) {
+Then("I select the options {string}, {string}, {string}", function (Team, Industry, Hear) {
+    cy.get('#teamId').select(Team);
+    cy.get('#industry').select(Industry);
+    cy.get('#hearAboutCYF').select(Hear);
+});
+
+Then("I click on the {string} and {string} as some experience", function (Guide, TeachPeopleCoding) {
     cy.get(':nth-child(10) > :nth-child(2) > .form-table-single > .form-check > .form-check-label').click();
     cy.get('[for="media98y439hf934Some"]').click();
     cy.get('#media23edcs3h3j3').click();
@@ -43,10 +41,10 @@ Then('I submit the form', () => {
     cy.get('.volunteer-submit-btn').click();
 });
 
-Then("I should see {string}", function (string) {
+Then("I should see {string}", function (ConfirmationMessage) {
     cy.get('#root > div > div.main > div > p').then(($p) => {
         const text = $p.text();
         console.log(text);
-        expect(text).to.include(string);
+        expect(text).to.include(ConfirmationMessage);
     })
 });
