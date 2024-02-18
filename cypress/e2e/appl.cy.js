3132313ee6a5161e43f2dc56944d425ae55bf0ee
enderta <endertanver@gmail.com>
2024-02-18 23:37:40 +0000

    updated

---
+++ cypress/e2e/appl.cy.js
@@ -0,0 +1,28 @@
+describe('getApplicantRegistrationWelcomeEmailTemplate', () => {
+    let token;
+    before(() => {
+        cy.generateToken().then((result) => {
+            token = result;
+        })
+    })
+
+    const checkMessage = (date, expectedMessage) => {
+        const now= new Date(date)
+        cy.clock(now, ['Date'])
+        cy.visit('http://localhost:3002/log-in/'+token)
+        cy.get('.message-bar > h4').should('contain', expectedMessage)
+    }
+
+    it('30 days message', () => {
+        checkMessage('2024-02-18T15:53:37.316Z', '30');
+    })
+    it('20 days message', () => {
+        checkMessage('2024-02-28T15:53:37.316Z', '20');
+    })
+    it('10 days message', () => {
+        checkMessage('2024-03-09T15:53:37.316Z', '10');
+    })
+    it('0 days message', () => {
+        checkMessage('2024-03-19T15:53:37.316Z', '0');
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,59 @@
 const dotenv = require('dotenv');
 dotenv.config({path: './cypress.config.js'});
 require('@testing-library/cypress/add-commands');
+import 'cypress-real-events/support'
 
-Cypress.Commands.add('logInToDashboard', () => {
+Cypress.Commands.add('generateToken', () => {
     const id = Cypress.env("ADMIN_ID");
     cy.exec(`cd ${Cypress.env('CYF_API_PATH')} && yarn generate-jwt ${id} 100d`).then((result) => {
         return result.stdout.split('\n').find(line => line.startsWith('eyJ'));
     })
 })
 
-
 Cypress.Commands.add('logOutOfDashboard', () => {
+    cy.reload()
     cy.get('.dropdown.media-display-none > .dropdown-toggle').click()
     cy.get('#navbarText > div > div > div > span').click()
     cy.get('.mt-5').should('contain', 'Please sign in to continue')
+})
+
+Cypress.Commands.add('loginDashboard', () => {
+    cy.generateToken().then((result) => {
+        cy.visit('http://localhost:3000/log-in/' + result)
+    })
+    const email = Cypress.env("ADMIN_EMAIL");
+    cy.get('.dropdown.media-display-none > .dropdown-toggle').click()
+    cy.get('[href="/profile"]').click({force: true})
+    cy.get('.row > :nth-child(2)').should('contain', email)
+})
+
+Cypress.Commands.add('loginApplicationProcess', () => {
+    cy.generateToken().then((token) => {
+        cy.visit('http://localhost:3002/log-in/'+token)
+    })
+})
+
+Cypress.Commands.add('createCohort', (region, number, registrationEndDate, itdStartDate, itdEndDate, sdcStartDate, alertMessage) => {
+    cy.get('.navbar-nav.media-display-none > [href="/cohorts"]').click()
+    cy.get('.btn').click({force: true})
+    cy.get('#cohort-regions')
+        .type(region)
+        .realType('{enter}')
+    cy.get('#cohort-number')
+        .type(number)
+        .realType('{enter}')
+    cy.get('#itd_registration_end_date')
+        .type(registrationEndDate)
+        .realType('{enter}')
+    cy.get('#itd-start-date')
+        .type(itdStartDate)
+        .realType('{enter}')
+    cy.get('#itd-end-date')
+        .type(itdEndDate)
+        .realType('{enter}')
+    cy.get('#sdc-start-date')
+        .type(sdcStartDate)
+        .realType('{enter}')
+    cy.get('.add-btn').click()
+    cy.get('.alert').should('contain', alertMessage)
 })
